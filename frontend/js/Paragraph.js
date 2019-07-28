4feb598087f947452e246560ae299fc5be791f57
Michael Zhen <m3zhen@uwaterloo.ca>
2019-07-28 16:40:03 -0400

    Delete Elements

--- frontend/js/Paragraph.js
+++ frontend/js/Paragraph.js
@@ -1,8 +1,7 @@
 import { Element, builderApp } from './Element.js';
 
 class Paragraph extends Element{
-    readyDelete = false;
-
+    
     constructor(){
         let textToAdd = prompt("Please enter the text that will be displayed.");
         let newParagraphElement = document.createElement('p');
@@ -12,28 +11,6 @@ class Paragraph extends Element{
         this.newParagraphElement = newParagraphElement;
         this.newParagraphElement.innerHTML = this.textToAdd;
         this.newParagraphElement.id = "resize-test2";
-        newParagraphElement.addEventListener("click", () => {
-            this.readyDelete = !this.readyDelete;
-
-            if(this.readyDelete){
-                // this.newParagraphElement.id = "delete-border";
-                this.newParagraphElement.className = "testClass";
-                console.log("ELEMENT DETAILS ", this.newParagraphElement);
-                builderApp.deleteElements.push(this);
-            }
-            else{
-                for(var i = 0; i < builderApp.deleteElements.length; i++){
-                    if(builderApp.deleteElements[i] == this){
-                        builderApp.deleteElements.splice(i,1);
-                    }
-                }
-                this.newParagraphElement.id = "resize-test2";
-            }
-        });
-    }
-
-    Delete() {
-        this.newParagraphElement.remove();
     }
 }
 
